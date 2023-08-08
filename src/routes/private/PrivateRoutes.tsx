07b2189cf6cdd3505bb8ebadc424b64983058957
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-08-09 01:08:44 +0530

    setup Auth Crendtal

---
+++ src/routes/private/PrivateRoutes.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PrivateRoutes = () => {
+    return (
+        <View>
+            <Text>PrivateRoutes</Text>
+        </View>
+    )
+}
+
+export default PrivateRoutes
+
+const styles = StyleSheet.create({})
