02cfb4278ed3da688d0beafb34a6d9b06531627a
Pratyush Kumar <pratyushkumar768@gmail.com>
2023-08-16 21:43:10 +0530

    Install Navigation package

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { PrivateRoutes } from './routes/private'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {PrivateRoutes} from './routes/private';
+import {Login} from './screen/auth';
 
 const Routes = () => {
-    return (
-        <PrivateRoutes />
-    )
-}
+  return (
+    // <PrivateRoutes />
+    <Login />
+  );
+};
 
-export default Routes
+export default Routes;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
